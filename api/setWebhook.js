084fc73bdbb74b7a27b2b0f43920b5c6f6512782
Sergey <sergbfmv@mail.ru>
2024-02-14 17:39:25 +0300

    fix bugs

--- api/setWebhook.js
+++ api/setWebhook.js
@@ -1,6 +1,6 @@
 import {setWebhook} from "telebot-vercel"
 import bot from "../index"
 
-const path = "api/telegram.mjs"
+const path = "api/telegram.js"
 
 export default setWebhook({bot, path, handleErrors: true})
